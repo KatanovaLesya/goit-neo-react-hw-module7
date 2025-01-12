496e20b0bc5aa74711fbe41556ccf54622455afc
KatanovaLesya <katanovalesya@gmail.com>
2025-01-12 23:10:28 +0200

    +

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
   build: {
     rollupOptions: {
       external: ['react', 'react-dom'], 
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src', 
     },
   },
 });
